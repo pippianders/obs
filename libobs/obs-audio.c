dae731dd7e0a1704cda15d1a534a26208a4d270b
jp9000 <obs.jim@gmail.com>
2016-01-12 01:40:59 -0800

    fixup! libobs: Implement new audio subsystem

--- libobs/obs-audio.c
+++ libobs/obs-audio.c
@@ -50,7 +50,7 @@ static inline void mix_audio(struct audio_output_data *mixes,
 	size_t total_floats = AUDIO_OUTPUT_FRAMES;
 	size_t start_point = 0;
 
-	if (ts->end <= source->audio_ts)
+	if (source->audio_ts < ts->start || ts->end <= source->audio_ts)
 		return;
 
 	if (source->audio_ts != ts->start) {
